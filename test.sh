4fabf9c5df4aebde4d21344f2e9e9466ac08583b
Gerhard Lipp <gelipp@gmail.com>
2013-12-12 21:18:20 +0100

    new branch for debugging and fixing travis tests

--- test.sh
+++ test.sh
@@ -2,9 +2,16 @@
 killall wstest 2>/dev/null
 wstest -m echoserver -w ws://localhost:${LUAWS_WSTEST_PORT:=8081}  &
 pid=$!
-echo "Waiting for wstest to start..."
-sleep 10
-busted spec/
+for T in {1..2}
+do
+    echo "Waiting for wstest to start..." $T "/10 secs"
+    sleep 1
+done
+for spec in `find spec -name "*_spec.lua"`
+do
+  echo "running busted" $spec
+  busted -o TAP $spec
+done
 bustedcode=$?
 kill ${pid}
 exit $bustedcode
